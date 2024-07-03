1577cacd145551717e4d6ab8eccb00eabcb73eb8
Aswin Asok <aswinasokofficial@gmail.com>
2024-07-03 08:47:34 +0530

    documented

--- dijsktras.c
+++ dijsktras.c
@@ -5,12 +5,28 @@ void main()
     int n;
     printf("Enter the number of vertices: ");
     scanf("%d", &n);
+
+    /*
+    vertexDistances is used to store the distance from one vertex to another,
+    where the row indicates the current vertex the column indicates another vertex.
+    and each cell has the value of the distance between them(0 for no edge)
+    */
     int vertexDistances[n][n];
 
+    /*
+    for calulating the distances we need another similar matrix also for storing
+    the relaxed distance as per Abdul Bari Sir's video.
+    */
     int calculationArray[n][n];
+
+    /*
+    An vertex which has been once scanned don't need to be scanned again, for keeping
+    track of that we use this 1d array which pushes each of the shortest vertex.
+    */
     int scannedVertices[n];
     int scannedVerticesIndex = 0;
 
+    // Taking user inputs.
     for (int i = 0; i < n; i++)
     {
         printf("Enter the distance from %d to other vertices\n", i);
@@ -21,11 +37,13 @@ void main()
         }
     }
 
+    // Printing the user input as well as initalizing the first row of the calculationArray.
     printf("Vertex Distances:\n");
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
+            // we are initalizing the first row only so i == 0 condition check.
             if (i == 0)
                 calculationArray[i][j] = vertexDistances[i][j];
             printf("%d\t", vertexDistances[i][j]);
@@ -33,14 +51,25 @@ void main()
         printf("\n");
     }
 
+    // A variable to keep track of the calculationArrayIndex.
     int calculationArrayIndex = 1;
+
+    /*
+    for each row iteration we will find the shortest edge amongst
+    the current row values and that vertex will be travelled next.
+    nextShortestVertex is used to store that vertex number.
+    */
     int nextShortestVertex = 0;
 
     for (int i = 0; i < n; i++)
     {
+        // reinializing the nextShortestVertex to 0, for each row its recalculated.
         nextShortestVertex = 0;
+
+        // by default this prmg sets source as 1st vertext(aka 0 as per program), thus j starts with 1.
         for (int j = 1; j < n; j++)
         {
+            // checking whether that vertex has been already scanned, if so updating the flag.
             int alreadyScanned = 0;
             for (int k = 0; k < scannedVerticesIndex; k++)
             {
@@ -50,6 +79,8 @@ void main()
                     break;
                 }
             }
+
+            // else storing the vertex number to our variable, provided it satisfyies the conditions.
             if (alreadyScanned == 0)
                 if (calculationArray[i][nextShortestVertex] == 0 || (calculationArray[i][j] > 0 && calculationArray[i][j] < calculationArray[i][nextShortestVertex]))
                 {
@@ -57,7 +88,17 @@ void main()
                 }
         }
 
+        // pushing the unscanned vertex to the scannedVerticesArray for later use.
         scannedVertices[scannedVerticesIndex++] = nextShortestVertex;
+
+        // calculating the distance via new shortest edge and replacing old distance if less than.
+
+        /*
+        CORE FORMULA
+
+        if(d[u] + c(c,v) < d[v])
+            d[v] = d[u] + c(c,v)
+        */
 
         for (int k = 1; k < n; k++)
         {
@@ -77,6 +118,8 @@ void main()
         calculationArrayIndex++;
     }
 
+    // printing the shortest path from the source vertex.
+    // j starts from 1 as source is 0.
     printf("\n\nVertex \t Distance from Source\n");
 
     for (int j = 1; j < n; j++)
